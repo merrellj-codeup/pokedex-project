5620fa3ba47c86c8c94c335593059fa64db6aee3
SgtMerrell <74498850+SgtMerrell@users.noreply.github.com>
2023-10-25 17:12:56 -0500

    init

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 import { renderPokemon } from "./functions.js";
 
+const debounce = (func, delay) => {
+	let debounceTimer;
+	return function () {
+		const context = this;
+		const args = arguments;
+		clearTimeout(debounceTimer);
+		debounceTimer = setTimeout(() => func.apply(context, args), delay);
+	};
+};
+
 (async () => {
 	const id = new URLSearchParams(window.location.search).get("id");
+	const searchForm = document.querySelector("#search-form");
+	const searchBar = document.querySelector("#search");
 	const pokemonContainer = document.querySelector("#pokemon-container");
 	if (id) {
 		renderPokemon(id, pokemonContainer);
 	} else {
 		renderPokemon(1, pokemonContainer);
 	}
+	searchBar.addEventListener(
+		"keyup",
+		debounce(() => {
+			// submit search form
+			searchForm.submit();
+		}, 500)
+	);
 })();
