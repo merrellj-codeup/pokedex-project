361db217c85278163a9099369cc3db2da58f7652
SgtMerrell <74498850+SgtMerrell@users.noreply.github.com>
2023-10-25 16:34:00 -0500

    init

--- js/index.js
+++ js/index.js
@@ -2,11 +2,9 @@ import { renderPokemon } from "./functions.js";
 
 (async () => {
 	const id = new URLSearchParams(window.location.search).get("id");
-	const searchBar = document.querySelector("#search");
 	const pokemonContainer = document.querySelector("#pokemon-container");
 	if (id) {
 		const pokemonName = await renderPokemon(id, pokemonContainer);
-		searchBar.value = pokemonName;
 	} else {
 		renderPokemon(1, pokemonContainer);
 	}
